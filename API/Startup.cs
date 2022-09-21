27389ea6885e7384f241813da38536e223b38a9f
Ayse <aysesadioglu@protonmail.com>
2022-09-21 21:39:39 +0300

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -19,7 +19,7 @@ namespace API
 {
     public class Startup
     {
-        private  readonly IConfiguration _config;
+        private readonly IConfiguration _config;
 
         public Startup(IConfiguration config)
         {
@@ -36,11 +36,26 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
-            services.AddDbContext<DataContext>(opt=>{
+            services.AddDbContext<DataContext>(opt =>
+            {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
 
             });
+
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+
+                    policy.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+                });
+            });
         }
+
+
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -55,6 +70,8 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors("CorsPolicy");
+
 
             app.UseAuthorization();
 
